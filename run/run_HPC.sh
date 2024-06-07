218d8b3c517964c5cd91b6c8fefc4781028e17be
DDUtian <109941661+DDUtian@users.noreply.github.com>
2024-06-08 04:51:52 +0800

    Delete run/run_HPC.sh

--- run/run_HPC.sh
+++
@@ -1,3 +0,0 @@
-$PYTHON run/train.py $SOURCE_DOMAIN $TARGET_DOMAIN $DATA_PATH --batch_size $BATCH_SIZE --num_train_epochs $TRAIN_EPOCH --learning_rate 0.0001
-
-$PYTHON run/eval.py $SOURCE_DOMAIN $TARGET_DOMAIN $DATA_PATH --batch_size $BATCH_SIZE --eval_epoch 10
